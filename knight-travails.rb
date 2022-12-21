d257d007fb3c02d57e4bfc01e477dad7a8085997
Jay C <jc30g14@soton.ac.uk>
2022-12-21 01:55:45 +0000

    Added method to find next possible moves with level order traversal being best way of finding the final end position. However need to find a way of adding the parent moves once the final position is found.

--- knight-travails.rb
+++ knight-travails.rb
@@ -42,13 +42,50 @@ end
 def knight_moves(start_position, end_position)
   board = initialise_board
   knight_start_position = knight_board_position(start_position, board)
-  knight_start_position.nil? ? nil : knight_position
+  knight_start_position.nil? ? nil : knight_start_position
+  next_positions = next_moves(start_position)
+  # puts "#{next_positions}"
+  level_order_traversal(start_position, next_positions, end_position)
+end
+
+def next_moves(curr_position, start_position = nil)
+  row, column = curr_position
+  new_moves = []
+  (1..2).each do |i|
+    j = 1
+    i == 1 ? j = 2 : j = 1
+
+    if valid_move?(row + i, column + j) && [row + i, column + j] != start_position then new_moves.push([row + i, column + j]) end
+    if valid_move?(row + i, column - j) && [row + i, column - j] != start_position then new_moves.push([row + i, column - j]) end
+    if valid_move?(row - i, column + j) && [row - i, column + j] != start_position then new_moves.push([row - i, column + j]) end
+    if valid_move?(row - i, column - j) && [row - i, column - j] != start_position then new_moves.push([row - i, column - j]) end
+  end
+  new_moves
+end
+
+def level_order_traversal(start_position, first_moves, end_position)
+  queue = first_moves
+  current_moves = []
+  current_moves << start_position
+  until queue.empty?
+    puts "#{queue}"
+    check_move = queue.shift
+    puts "#{check_move}"
+    if check_move == end_position
+      current_moves << check_move
+      queue = []
+    else
+      #resume here, find way of adding all moves to current_moves array. Save parent nodes. 
+      child_moves = next_moves(check_move, start_position)
+      child_moves.each {|move| queue << move }
+    end
+  end
+  current_moves
 end
 
 
-def testing(start)
-  end_pos = [1, 2]
+def testing(start, end_pos)
   knight_moves(start, end_pos)
 end
 
-testing([-1, 0])
+testing([3, 3], [0, 0])
