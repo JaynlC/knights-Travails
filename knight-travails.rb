0491db797317914d2bed7a133b9393e9a727618b
Jay C <jc30g14@soton.ac.uk>
2022-12-22 01:20:34 +0000

    Used a Hash to store parent moves which is fed into array of all moves taken, as part of the evaluate_path method. But if more than 2 moves, the programme breaks, therefore need to find efficient method.

--- knight-travails.rb
+++ knight-travails.rb
@@ -64,28 +64,55 @@ def next_moves(curr_position, start_position = nil)
 end
 
 def level_order_traversal(start_position, first_moves, end_position)
+  parent_move = {}
+  first_moves.each do |move|
+    parent_move[move] = start_position
+  end
+  # puts "#{parent_move}"
   queue = first_moves
   current_moves = []
   current_moves << start_position
+
   until queue.empty?
-    puts "#{queue}"
+    # puts "#{queue}"
     check_move = queue.shift
-    puts "#{check_move}"
     if check_move == end_position
       current_moves << check_move
       queue = []
     else
-      #resume here, find way of adding all moves to current_moves array. Save parent nodes. 
       child_moves = next_moves(check_move, start_position)
-      child_moves.each {|move| queue << move }
+      child_moves.each do |move| 
+        unless parent_move.include?(move)
+          queue << move
+          parent_move[move] = check_move
+          # puts "#{parent_move}"
+        end
+      end
     end
   end
-  current_moves
+  evaluate_path(start_position, parent_move, end_position)
 end
 
+def evaluate_path(start_position, parent_move_hash, end_position)
+  all_moves = [end_position]
+  last_move = parent_move_hash.fetch(end_position)
+  all_moves << last_move
+  puts "#{last_move}"
+  p parent_move_hash
+  if last_move == start_position
+    all_moves.reverse
+  else
+    loop do
+      all_moves << parent_move_hash.fetch(last_move)
+      break if all_moves.last == start_position
+    end
+    all_moves.reverse
+  end
+end
 
 def testing(start, end_pos)
   knight_moves(start, end_pos)
 end
 
 testing([3, 3], [0, 0])
+testing([3, 3], [2, 1])
